ec239ddce25740065d6a277927f4bbe8b595f91b
abubakrmo <abubakarmufutauoseni@gmail.com>
2024-03-16 17:42:14 +0000

    title change in 'main' file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ASSIGNMENT ONE - SIMPLE GA IMPLEMENTATION");
+        System.out.println("GA IMPLEMENTATION");
     }
 }
